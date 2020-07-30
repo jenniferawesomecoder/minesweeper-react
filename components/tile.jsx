7fb4083a818c9cf326cbd3cdd0f37c4162200bf8
mumu2117 <jmillerarcher@me.com>
2020-07-30 13:35:49 -0400

    changed the handleclick to be an arrow function

--- components/tile.jsx
+++ components/tile.jsx
@@ -5,10 +5,10 @@ import React from 'react';
 class Tile extends React.Component{
   constructor(props){
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    
   }
   
-  handleClick(e) {
+  handleClick =(e) => {
     const flagged = e.altKey ? true : false;
     this.props.updateGame(this.props.tile, flagged);
   }
